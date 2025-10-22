80e8f9146636fe171ab91a53888f2384b2917b2a
zhangy84-web <zhangy84@uci.edu>
2025-10-21 20:59:52 -0700

    add miles1 tasks

---
+++ milestone1/task1.py
@@ -0,0 +1,15 @@
+import re
+import sys
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+filename = sys.argv[1]
+
+with open(filename, 'r', encoding='utf-8') as file:
+    html_doc = file.read()
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+# 1
+with open("pretty_" + filename, 'w', encoding='utf-8') as outfile:
+	outfile.write(soup.prettify())
